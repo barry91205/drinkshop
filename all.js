120aa643cc116389a88fb64e674706381aab3ec5
barry91205 <119376509+barry91205@users.noreply.github.com>
2023-09-06 22:59:00 +0800

    新增作品集-貓子茶

---
+++ all.js
@@ -0,0 +1,18 @@
+// $(document).ready(function() {
+//     $('.word').slideUp(500);
+//   });
+
+// $(document).ready(function() {
+    
+//     $(".word").css({ "opacity": 0, "margin-left": "100%" }); 
+//     $(".word").animate({
+//         opacity: 1,
+//         marginLeft: 0
+//     }, 1000); 
+// });
+
+$(document).ready(function() {
+    // 当页面加载完成时，将段落元素的透明度从 0 淡入到 1
+    $("p").fadeTo(1000, 1); // 持续时间为 1000 毫秒，目标透明度为 1
+});
+ 
